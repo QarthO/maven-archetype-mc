d4249c3bb64d5ec69c27d15973a23a88336b29d3
QarthO <qartho@gmail.com>
2024-03-01 13:37:16 -0500

    setup example listener, and metrics

---
+++ src/main/java/gg/quartzdev/qtemplateplugin/listeners/ExampleListener.java
@@ -0,0 +1,11 @@
+package gg.quartzdev.qtemplateplugin.listeners;
+
+import org.bukkit.event.Listener;
+
+public class ExampleListener implements Listener {
+
+    public ExampleListener(){
+
+    }
+
+}
--- src/main/java/gg/quartzdev/qtemplateplugin/qTemplatePlugin.java
+++ src/main/java/gg/quartzdev/qtemplateplugin/qTemplatePlugin.java
@@ -1,5 +1,9 @@
 package gg.quartzdev.qtemplateplugin;
 
+import gg.quartzdev.qtemplateplugin.listeners.ExampleListener;
+import gg.quartzdev.qtemplateplugin.util.QPlugin;
+import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class qTemplatePlugin extends JavaPlugin {
@@ -9,11 +13,22 @@ public final class qTemplatePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-
+        new QPlugin(instance);
+//        setupMetrics(2133);
+        registerListeners();
     }
 
     @Override
     public void onDisable() {
 
     }
+
+    public void setupMetrics(int pluginId){
+        Metrics metrics = new Metrics(instance, pluginId);
+    }
+
+    public void registerListeners(){
+        Bukkit.getPluginManager().registerEvents(new ExampleListener(), instance);
+    }
+
 }
