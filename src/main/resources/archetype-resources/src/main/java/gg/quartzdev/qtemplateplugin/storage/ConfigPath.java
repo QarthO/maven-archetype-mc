3bd87fccc844794ea7df73a289925ba19c6b5e30
Derek Comella <derek@getbacktobiz.com>
2024-11-26 06:25:51 -0500

    add unversioned files, use maven variables, .idea

---
+++ src/main/resources/archetype-resources/src/main/java/gg/quartzdev/qtemplateplugin/storage/ConfigPath.java
@@ -0,0 +1,20 @@
+package gg.quartzdev.qtemplateplugin.storage;
+
+public enum ConfigPath
+{
+
+
+    CHECK_UPDATES("check-updates");
+
+    private final String path;
+
+    ConfigPath(String path)
+    {
+        this.path = path;
+    }
+
+    public String get()
+    {
+        return path;
+    }
+}
